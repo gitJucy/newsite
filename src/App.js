d11211d77c620301eeba206411a7bf5cca0f5c40
gitJucy <johnrlucy84@gmail.com>
2017-12-15 13:50:19 -0500

    first changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React-Static</h1>
         </header>
         <p className="App-intro">
           This is going to be a test run using react static
