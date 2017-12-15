cdeb4b9ad4749dc4dccff6b0a336deb6d4eeed73
gitJucy <johnrlucy84@gmail.com>
2017-12-15 12:35:29 -0500

    first files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import logo from './logo.png'
+
+import './app.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React-Static</h1>
+        </header>
+        <p className="App-intro">
+          This is going to be a test run using react static
+        </p>
+      </div>
+    )
+  }
+}
+
+export default App
